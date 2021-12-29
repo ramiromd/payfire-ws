b989caa1cbc57f32ca8a0e1dbeeafd4957051c52
Ramiro Martínez D'Elía <ramiro.md90@gmail.com>
2021-12-29 18:54:08 -0300

    First commit ...

---
+++ src/main/java/com/ramiromd/payfire/PayfireApplication.java
@@ -0,0 +1,13 @@
+package com.ramiromd.payfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayfireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramiromd/payfire/PayfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramiromd.payfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayfireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
