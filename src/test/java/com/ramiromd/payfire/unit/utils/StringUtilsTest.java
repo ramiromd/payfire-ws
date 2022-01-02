cf76e80e83094a05667d182df55557b034680400
Ramiro Martínez D'Elía <ramiro.md90@gmail.com>
2022-01-02 16:52:17 -0300

    String utils with sluggify

---
+++ src/test/java/com/ramiromd/payfire/unit/utils/StringUtilsTest.java
@@ -0,0 +1,20 @@
+package com.ramiromd.payfire.unit.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ramiromd.payfire.utils.StringUtils;
+
+@SpringBootTest
+public class StringUtilsTest {
+	
+	
+	@Test
+	public void it_can_sluggify_a_string() {
+		
+		assertEquals("hello-world", StringUtils.toSlug("Hello World !!"));
+		assertEquals("nandu", StringUtils.toSlug("Ñandú"));
+	}
+}
